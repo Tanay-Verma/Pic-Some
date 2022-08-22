3e4545c3aeeb116f94671e2133caa5520fd948e3
Tanay <tanayverma98@gmail.com>
2022-08-22 19:17:06 +0530

    heartIcona and cartIcon now display when the imges are hovered over

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Image({ img, className }) {
+    // to see if the mouse is hovered over the images 
+    const [hovered,setHovered] = useState(false)
+
+    // changes the hovered state based on if the mouse is on top of images
+    const handleHover = ()=>{
+        setHovered(!hovered)
+    }
+
+    // these icons are displayed when hovered is flipped to true
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
         <div className={`${className} image-container`}>
-            <img src={img.url} className="image-grid" />
+            <img src={img.url} className="image-grid" onMouseEnter={handleHover} onMouseLeave={handleHover}/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
