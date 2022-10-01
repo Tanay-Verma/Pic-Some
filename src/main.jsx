d77af63e5913ba7e6f01d20e9c5e16b6781f27ce
Tanay <tanayverma98@gmail.com>
2022-10-01 20:29:35 +0530

    BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ContextProvider } from "./Context"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
